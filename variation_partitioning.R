7ce29bcfab1f39ba8ac5988e2f54ae7be59b7fbb
Vladimir Mikryukov <vmikk@users.noreply.github.com>
2023-03-01 13:24:19 +0200

    update varpart (Mohammad)

--- variation_partitioning.R
+++ variation_partitioning.R
@@ -1,19 +1,34 @@
 library(vegan)
 library(adespatial)
 
+
+space_fw <- forward.sel(y, space,nperm=999, alpha = 1,R2more= 0.01, verbose=F)   
+selected.space <- space[, space_fw$order]
+
+soil_fw <-forward.sel(y, soil,nperm=999, alpha = 1,R2more= 0.01, verbose=F) 
+selected.soil <- soil[, soil_fw$order]
+
+vegetation_fw <-forward.sel(y, vegetation,nperm=999, alpha = 1,R2more= 0.01,verbose=F)    
+selected.veg <- vegetation[, vegetation_fw$order]
+
+temporal_fw <-forward.sel(y, temporal,nperm=999, alpha = 1,R2more=0.01, verbose=F)   
+selected.temp <- temporal[, temporal_fw$order]
+
+vartpart_a.out <- varpart( y  ,selected.veg,selected.soil,selected.space,selected.temp)
+
+#########
+
+floristic_fw=forward.sel(y, floristic,nperm=999, alpha = 1,R2more= 0.01, verbose=F) #  
+selected.floristic = floristic[, floristic_fw$order]
+
+host_phylogeny_fw=forward.sel(y, host_phylogeny,nperm=999, alpha = 1,R2more= 0.01,verbose=F) #  
+selected.host_phylogeny= host_phylogeny[, host_phylogeny_fw$order]
+
+host_sp_fw=forward.sel(y, host_sp,nperm=999, alpha = 1,R2more=0.01, verbose=F) 
+selected.host_sp= host_sp[, host_sp_fw$order]
+
+others_fw=forward.sel(y, others,nperm=999, alpha = 1,R2more= 0.01, verbose=F)  
+selected.others= others[, others_fw$order]
  
-
-space_fw=forward.sel(y, x,nperm=999, alpha = 1,R2more= 0.01, verbose=F)    
-selected.space= x[, space_fw$order ]
-
-soil_fw=forward.sel(y, x,nperm=999, alpha = 1,R2more= 0.01, verbose=F)   
-selected.soil= x[, soil_fw$order ]
-
-vegetation_fw=forward.sel(y, x,nperm=999, alpha = 1,R2more= 0.01,verbose=F)   
-selected.veg= x[, vegetation_fw$order ]
-
-
-temporal_fw=forward.sel(y, x,nperm=999, alpha = 1,R2more=0.01, verbose=F) 
-selected.temp= x[, temporal_fw$order ]
-
-vartpart.out <- varpart( y  ,selected.veg,selected.soil,selected.space,selected.temp)
+vartpart_b.out =varpart(y, selected.host_sp, selected.floristic, selected.host_phylogeny,selected.others)
+ 
